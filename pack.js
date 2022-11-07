717bd68a7f04ce1b1876bc312b77e195c9765496
Haruma <114157573+harumatool-admin@users.noreply.github.com>
2022-11-07 14:12:43 +0700

    Add files via upload

--- pack.js
+++ pack.js
@@ -14,11 +14,38 @@ var HarumaToolKit = function () {
                         console.log('1. Pack Resources.json with fixed slots and shadow bugs [Slow]');
                         console.log('2. Pack Resources.json and rewrite [Fast]');
                         rl.question('Bool: ', function (chooseFunc) {
-                            packResources(filedir, chooseFunc);
+                            if (parseInt(chooseFunc) == 2) {
+                                rl.question('Would you like to delete 384 in subgroups ? ', function (delete384) {
+                                    rl.question('Would you like to delete 768 in subgroups ? ', function (delete768) {
+                                        rl.question('Would you like to delete 1536 in subgroups ? ', function (delete1536) {
+                                            packResources(filedir, chooseFunc, delete384, delete768, delete1536);
+                                        })
+                                    })
+                                })
+                            }
                         })
                     });
-                    var packResources = function (filedir, chooseFunc) {
+                    var deleteSubgroups = function(formatTexture, ResourcesGroup){
+                        try {
+                            for (let k = 0; k < ResourcesGroup.length; k++) {
+                                if (ResourcesGroup[k].subgroups != undefined) {
+                                    for (let p = 0; p < ResourcesGroup[k].subgroups.length; p++) {
+                                        if ((ResourcesGroup[k].subgroups[p].id).indexOf(formatTexture) != -1) {
+                                            (ResourcesGroup[k].subgroups).splice(p, 1)
+                                        }
+                                    }
+                                }
+                            }
+                            return
+                        } catch (error) {
+                            console.log(error)
+                        }
+                    }
+                    var packResources = function (filedir, chooseFunc, delete384, delete768, delete1536) {
                         const worker = parseInt(chooseFunc);
+                        const worker_384 = '_384';
+                        const worker_768 = '_768';
+                        const worker_1536 ='_1536';
                         const dir = './' + filedir + '/';
                         console.time("Done function in ");
                         const fs = require('fs');
@@ -31,7 +58,7 @@ var HarumaToolKit = function () {
                             files.forEach(file => {
                                 jsons.push(file);
                             });
-                            jsons = jsons.sort()                            
+                            jsons = jsons.sort()
                             for (let i = 0; i < jsons.length; i++) {
                                 let filepath = dir + jsons[i];
                                 fs.readFile(filepath, 'utf8', (err, data) => {
@@ -79,29 +106,29 @@ var HarumaToolKit = function () {
                                                                     ResourcesGroup[k].resources[m]['#comment'] = undefined;
                                                                 }
                                                                 try {
-                                                                    if ((ResourcesGroup[k].resources[m].width <0)) {
-                                                                        (ResourcesGroup[k].resources[m].width) = parseInt((ResourcesGroup[k].resources[m].width)*(-1));
+                                                                    if ((ResourcesGroup[k].resources[m].width < 0)) {
+                                                                        (ResourcesGroup[k].resources[m].width) = parseInt((ResourcesGroup[k].resources[m].width) * (-1));
                                                                     };
-                                                                    if ((ResourcesGroup[k].resources[m].height <0)) {
-                                                                        (ResourcesGroup[k].resources[m].height) = parseInt((ResourcesGroup[k].resources[m].height)*(-1));
+                                                                    if ((ResourcesGroup[k].resources[m].height < 0)) {
+                                                                        (ResourcesGroup[k].resources[m].height) = parseInt((ResourcesGroup[k].resources[m].height) * (-1));
                                                                     };
-                                                                    if ((ResourcesGroup[k].resources[m].ax <0)) {
-                                                                        (ResourcesGroup[k].resources[m].ax) = parseInt((ResourcesGroup[k].resources[m].ax)*(-1));
+                                                                    if ((ResourcesGroup[k].resources[m].ax < 0)) {
+                                                                        (ResourcesGroup[k].resources[m].ax) = parseInt((ResourcesGroup[k].resources[m].ax) * (-1));
                                                                     };
-                                                                    if ((ResourcesGroup[k].resources[m].ay <0)) {
-                                                                        (ResourcesGroup[k].resources[m].ay) = parseInt((ResourcesGroup[k].resources[m].ay)*(-1));
+                                                                    if ((ResourcesGroup[k].resources[m].ay < 0)) {
+                                                                        (ResourcesGroup[k].resources[m].ay) = parseInt((ResourcesGroup[k].resources[m].ay) * (-1));
                                                                     };
-                                                                    if ((ResourcesGroup[k].resources[m].aw <0)) {
-                                                                        (ResourcesGroup[k].resources[m].aw) = parseInt((ResourcesGroup[k].resources[m].aw)*(-1));
+                                                                    if ((ResourcesGroup[k].resources[m].aw < 0)) {
+                                                                        (ResourcesGroup[k].resources[m].aw) = parseInt((ResourcesGroup[k].resources[m].aw) * (-1));
                                                                     };
-                                                                    if ((ResourcesGroup[k].resources[m].ah <0)) {
-                                                                        (ResourcesGroup[k].resources[m].ah) = parseInt((ResourcesGroup[k].resources[m].ah)*(-1));
+                                                                    if ((ResourcesGroup[k].resources[m].ah < 0)) {
+                                                                        (ResourcesGroup[k].resources[m].ah) = parseInt((ResourcesGroup[k].resources[m].ah) * (-1));
                                                                     };
-                                                                    if ((ResourcesGroup[k].resources[m].x <0)) {
-                                                                        (ResourcesGroup[k].resources[m].x) = parseInt((ResourcesGroup[k].resources[m].x)*(-1));
+                                                                    if ((ResourcesGroup[k].resources[m].x < 0)) {
+                                                                        (ResourcesGroup[k].resources[m].x) = parseInt((ResourcesGroup[k].resources[m].x) * (-1));
                                                                     };
-                                                                    if ((ResourcesGroup[k].resources[m].y <0)) {
-                                                                        (ResourcesGroup[k].resources[m].y) = parseInt((ResourcesGroup[k].resources[m].y)*(-1));
+                                                                    if ((ResourcesGroup[k].resources[m].y < 0)) {
+                                                                        (ResourcesGroup[k].resources[m].y) = parseInt((ResourcesGroup[k].resources[m].y) * (-1));
                                                                     }
                                                                 } catch (error) {
                                                                     console.log(error)
@@ -118,9 +145,17 @@ var HarumaToolKit = function () {
                                                         } catch (error) {
                                                             console.log(error);
                                                         }
-                                                        
                                                     }
                                                 }
+                                            }
+                                            if(parseInt(delete384) == 1){
+                                                deleteSubgroups(worker_384, ResourcesGroup)
+                                            }
+                                            if(parseInt(delete768) == 1){
+                                                deleteSubgroups(worker_768, ResourcesGroup)
+                                            }
+                                            if(parseInt(delete1536) == 1){
+                                                deleteSubgroups(worker_1536, ResourcesGroup)
                                             }
                                             var resources = '{"version":1,"slot_count":0,"groups":[]}';
                                             try {
